4954f9ba59ba74632274bf574a6773095067b084
Hasan Huseyin Yakut <hasanhuseyinyakut@gmail.com>
2025-07-10 03:04:20 +0300

    Weather Forecast kodları silindi

--- ProductService/Program.cs
+++ ProductService/Program.cs
@@ -30,25 +30,7 @@ app.UseHttpsRedirection();
 // Use CORS
 app.UseCors();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
 
 // In-memory product list
 var products = new List<Product>
@@ -91,7 +73,3 @@ app.MapDelete("/products/{id}", (int id) =>
 app.Run();
 
 record Product(int Id, string Name, decimal Price);
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
--- UserService/Program.cs
+++ UserService/Program.cs
@@ -30,25 +30,7 @@ app.UseHttpsRedirection();
 // Use CORS
 app.UseCors();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
 
 // User Model
 // In-memory user list
@@ -92,7 +74,3 @@ app.MapDelete("/users/{id}", (int id) =>
 app.Run();
 
 record User(int Id, string Name, string Email);
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
